940d940ada2c3a88be1c3656999355e877b2029c
AmorFatiBA <you@example.com>
2020-07-26 14:45:33 +0300

    mod

--- indexer.cs
+++ indexer.cs
@@ -19,7 +19,7 @@ namespace Calisma_indexer
     class CokBoyutluIndexer
     {
         private int[,] dizi1;
-        public CokBoyutluIndexer(int Boyut1, int Boyut2)
+        public CokBoyutluIndexer(int Boyut1, int Boyut2) //
         {
             dizi1 = new int[Boyut1, Boyut2];
         }
@@ -49,7 +49,39 @@ namespace Calisma_indexer
             }
         }
 
-
+    }
+    class CokBoyutluIndekser
+    {
+        private int[,] dizi;
+        public CokBoyutluIndekser(int Boyut1, int Boyut2)
+        {
+            dizi = new int[Boyut1, Boyut2];
+        }
+        public int Boyut1
+        {
+            get
+            {
+                return dizi.GetLength(0);
+            }
+        }
+        public int Boyut2
+        {
+            get
+            {
+                return dizi.GetLength(1);
+            }
+        }
+        public int this[int indeks1, int indeks2]
+        {
+            get
+            {
+                return dizi[indeks1, indeks2];
+            }
+            set
+            {
+                dizi[indeks1, indeks2] = (int)Math.Sqrt(value);
+            }
+        }
 
     }
     class IndeksIslem
@@ -92,7 +124,7 @@ namespace Calisma_indexer
             }
             set
             {
-                sayi = value;
+               sayi= value*value;
             }
         }
 
@@ -117,25 +149,25 @@ namespace Calisma_indexer
         static void Main(string[] args)
         {
 
-        //     class Indexers
-        // {
-        //     public double sayi;
-        //     public double this[double indeks]
-        //     {
-        //         get
-        //         {
-        //             return indeks * indeks;//sayi;//
-        //         }
-        //         set
-        //         {
-        //             sayi = value;
-        //         }
-        //     }
+            //     class Indexers
+            // {
+            //     public double sayi;
+            //     public double this[double indeks]
+            //     {
+            //         get
+            //         {
+            //             return indeks * indeks;//sayi;//
+            //         }
+            //         set
+            //         {
+            //             sayi = value;
+            //         }
+            //     }
 
 
-        // }
-        Indexers indeks = new Indexers();
-        Console.WriteLine("i[1.2]={0}", indeks[1.2]);
+            // }
+            Indexers indeks = new Indexers();
+            Console.WriteLine("i[1.2]={0}", indeks[1.2]);
             indeks[5] = 2 * indeks[1.2];
             Console.WriteLine(indeks.sayi);
             indeks[6] = 9;
@@ -144,20 +176,22 @@ namespace Calisma_indexer
             indeks[8] = 8;
             Console.WriteLine(indeks[7].Equals(indeks[6]));
             Console.WriteLine(indeks.sayi.Equals(indeks.sayi));
-            
+
             Console.WriteLine(indeks.sayi);
             Console.WriteLine("i[5]={0}", indeks[5]);
             Console.WriteLine("i[6]={0}", indeks[6]);
             Console.WriteLine("i[7]={0}", indeks[7]);
             Console.WriteLine("i[8]={0}", indeks[8]);
 
+            
+
             IndeksIslem a = new IndeksIslem(7); // Burada dinamik boyutlu diziler tanımlıyoruz
-        IndeksIslem b = new IndeksIslem(8);
+            IndeksIslem b = new IndeksIslem(8);
 
-        int[] z = new int[5];
-        DiziYaz(z, BICIM.DIKEY);
+            int[] z = new int[5];
+            DiziYaz(z, BICIM.DIKEY);
 
-            for (int j = 0; j<a.DiziBoyut; ++j) // burada DiziBoyut "Lenght" metodunun görevini görür bunu biz 
+            for (int j = 0; j < a.DiziBoyut; ++j) // burada DiziBoyut "Lenght" metodunun görevini görür bunu biz 
                                                   // metot olarak IndeksIslem sınıfında tanımladık
             {
                 a[j] = j + 1; // --> value => dizi[indeks] burada j+1 değerini x[j] ye atar
@@ -169,30 +203,51 @@ namespace Calisma_indexer
 
 
 
-    CokBoyutluIndexer c = new CokBoyutluIndexer(10, 7);
+            CokBoyutluIndexer c = new CokBoyutluIndexer(10,8);
 
-            for (int y = 0; y<c.Boyut1; ++y)
-                for (int x = 0; x<c.Boyut2; ++x)
+            for (int y = 0; y < c.Boyut1; ++y)
+                for (int x = 0; x < c.Boyut2; ++x)
                     c[y, x] = y + x;
+                    
 
-            for (int y = 0; y<c.Boyut1; ++y)
+            for (int y = 0; y < c.Boyut1; ++y)
             {
-                for (int x = 0; x<c.Boyut2; ++x)
+                for (int x = 0; x < c.Boyut2; ++x)
                     Console.Write("{0,4} ", c[y, x]);
                 Console.WriteLine();
 
             }
 
-string[] Gunler = HaftaninGunleri.GetNames(typeof(HaftaninGunleri));
-foreach (string g in Gunler)
-    Console.WriteLine(g);
+            string[] Gunler = HaftaninGunleri.GetNames(typeof(HaftaninGunleri));
+            foreach (string g in Gunler)
+                Console.WriteLine(g);
 
 
-string[] bicim = BICIM.GetNames(typeof(BICIM));
-foreach (string g in bicim)
-    Console.WriteLine("\n" + g);
+            string[] bicim = BICIM.GetNames(typeof(BICIM));
+            foreach (string g in bicim)
+                Console.WriteLine("\n" + g);
 
-                
+            CokBoyutluIndekser cb = new CokBoyutluIndekser(5,4);
+            for (int i = 0; i < cb.Boyut1; ++i)
+                for (int j = 0; j < cb.Boyut2; ++j)
+                    cb[i, j] = i * j;
+                    
+                    
+                    
+
+            for (int i = 0; i < cb.Boyut1; ++i)
+            {
+                for (int j = 0; j < cb.Boyut2; ++j)
+                {
+                    Console.Write("{0,3}", cb[i, j]);
+                }
+                Console.WriteLine();
+            }
+            int [,] bi = new int[2,2];
+            Console.WriteLine(bi.GetLength(0));
+            
+
+
 
 
 
